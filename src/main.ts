20797d35da4ec33d3af788ff28494e287e16b725
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-01-01 20:23:31 +0200

    Add src/1.pdf and pdf-parse dependency

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello World!')
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+
+export const run = async () => {
+  const loader = new PDFLoader("src/1.pdf");
+
+  const docs = await loader.load();
+
+  console.log(process.env.STAGE)
+  console.log({ docs });
+};
+
+run();
+
