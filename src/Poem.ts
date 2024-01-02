55694277c084c8cb8aab4344be27146a208690e9
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-01-02 13:13:04 +0200

    Add openai dependency and update start script

--- src/Poem.ts
+++ src/Poem.ts
@@ -1,2 +1,16 @@
-const key = ''
+import { OpenAI } from 'langchain/llms/openai';
+
+async function execute(){
+    const model = new OpenAI({
+        openAIApiKey: process.env.openapi_key
+    })
+    const response = await model.call(
+        'Give me some Ron Swanson quotes in JSON format',
+    );
+    console.log(response);
+}
+
+
+
+execute();
 
