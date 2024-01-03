e1742ee16dfb1557748dbecb4877fb6c1ea4d595
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-01-03 16:23:52 +0200

    Add launch configuration and message history saving

--- src/Dialog.ts
+++ src/Dialog.ts
@@ -1,22 +1,29 @@
 import { ChatOpenAI } from 'langchain/chat_models/openai';
 import { LLMChain } from 'langchain/chains'
 import { HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder } from 'langchain/prompts';
-import { ConversationSummaryBufferMemory } from 'langchain/memory';
+import { ConversationSummaryBufferMemory, ChatMessageHistory } from 'langchain/memory';
+import { readFileSync, existsSync, writeFileSync } from 'fs'
+
+
+// NOT WORKING MESSAGE HISTORY LOADING FROM JSON!!!
+
 
 const prompt = new ChatPromptTemplate({
-    inputVariables:['content', 'messages'],
-    promptMessages:[
+    inputVariables: ['content', 'messages'],
+    promptMessages: [
         new MessagesPlaceholder('messages'),
         HumanMessagePromptTemplate.fromTemplate('{content}')
     ],
 })
 
 const chatOpenAI = new ChatOpenAI();
+const messageHistory = new ChatMessageHistory();
 
 const memory = new ConversationSummaryBufferMemory({
-    memoryKey:'messages',
+    memoryKey: 'messages',
     returnMessages: true,
-    llm:chatOpenAI
+    llm: chatOpenAI,
+    chatHistory: messageHistory
 });
 
 const chain = new LLMChain({
@@ -25,11 +32,29 @@ const chain = new LLMChain({
     memory: memory,
 });
 
-process.stdin.addListener('data', async function(input) {
+process.stdin.addListener('data', async function (input) {
+    // await loadMessageHistory();
     let userInput = input.toString().trim();
 
     const result = await chain.call({
         content: userInput
     });
     console.log(result.text);
+    await saveMessageHistory();
 });
+
+async function loadMessageHistory() {
+    if (existsSync('messages.json')) {
+        const file = readFileSync('messages.json')
+        const messages = JSON.parse(file.toString());
+        for (const message of messages) { // Fix the type of 'message' variable
+            await messageHistory.addMessage(message);
+        }
+    }
+}
+
+async function saveMessageHistory() {
+    const messages = await messageHistory.getMessages();
+    const file = JSON.stringify(messages);
+    writeFileSync('messages.json', file);
+}
