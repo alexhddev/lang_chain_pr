94922cd3728db59081789604bea7b66cf47f2e55
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-01-08 16:27:51 +0200

    Working chatbot with openai

---
+++ src/OpenAiDiag.ts
@@ -0,0 +1,36 @@
+import OpenAI from 'openai'
+import { readFileSync, existsSync, writeFileSync } from 'fs'
+
+const openai = new OpenAI();
+
+const history: OpenAI.Chat.ChatCompletionMessageParam[] = [
+    {
+        role: 'system',
+        content: 'You are a very cool AI Chatbot. You answer like a bro'
+    }
+];
+console.log('Hello from cool chatbot!')
+process.stdin.addListener('data', async function (input) {
+    // await loadMessageHistory();
+    let userInput = input.toString().trim();
+    history.push({
+        role: 'assistant',
+        content: userInput
+    })
+    await createChatCompletion();
+});
+
+async function createChatCompletion(){
+    const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        temperature: 0,
+        messages: history
+    })
+    const responseMessage = response.choices[0].message;
+    const messageParam: OpenAI.Chat.ChatCompletionMessageParam = {
+        role:responseMessage.role,
+        content: responseMessage.content
+    }
+    history.push(messageParam);
+    console.log(`${messageParam.role}: ${messageParam.content}`)
+}
